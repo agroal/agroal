cb4dff099ca29828d82d87c9277578de80f98a14
barreiro <lbbbarreiro@gmail.com>
2019-02-05 03:21:45 +0000

    AG-105 - Kerberos support

---
+++ agroal-api/src/main/java/io/agroal/api/security/AgroalKerberosSecurityProvider.java
@@ -0,0 +1,34 @@
+// Copyright (C) 2017 Red Hat, Inc. and individual contributors as indicated by the @author tags.
+// You may not use this file except in compliance with the Apache License, Version 2.0.
+
+package io.agroal.api.security;
+
+import org.ietf.jgss.GSSCredential;
+import org.ietf.jgss.GSSException;
+import org.ietf.jgss.Oid;
+
+import java.util.Properties;
+
+/**
+ * @author <a href="lbarreiro@redhat.com">Luis Barreiro</a>
+ */
+public class AgroalKerberosSecurityProvider implements AgroalSecurityProvider {
+
+    private static final Properties EMPTY_PROPERTIES = new Properties();
+
+    private static final String KERBEROS_v5 = "1.2.840.113554.1.2.2";
+
+    @Override
+    public Properties getSecurityProperties(Object securityObject) {
+        if ( securityObject instanceof GSSCredential ) {
+            try {
+                Properties properties = new Properties();
+                properties.setProperty( "user", ( (GSSCredential) securityObject ).getName( new Oid( KERBEROS_v5 ) ).toString() );
+                return properties;
+            } catch ( GSSException e ) {
+                // nothing we can do
+            }
+        }
+        return EMPTY_PROPERTIES;
+    }
+}
