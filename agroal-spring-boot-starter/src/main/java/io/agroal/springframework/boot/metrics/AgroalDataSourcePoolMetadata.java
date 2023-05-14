48b5da20890545b3341a80c1977b5df171eb25fe
Benjamin Graf <benjamin.graf@gmx.net>
2023-05-14 17:04:01 +0200

    Add DataSourcePoolMetadataProvider implementation

---
+++ agroal-spring-boot-starter/src/main/java/io/agroal/springframework/boot/metrics/AgroalDataSourcePoolMetadata.java
@@ -0,0 +1,45 @@
+// Copyright (C) 2020 Red Hat, Inc. and individual contributors as indicated by the @author tags.
+// You may not use this file except in compliance with the Apache License, Version 2.0.
+
+package io.agroal.springframework.boot.metrics;
+
+import org.springframework.boot.jdbc.metadata.AbstractDataSourcePoolMetadata;
+
+import io.agroal.springframework.boot.AgroalDataSource;
+
+public class AgroalDataSourcePoolMetadata extends AbstractDataSourcePoolMetadata<AgroalDataSource> {
+
+    public AgroalDataSourcePoolMetadata(AgroalDataSource dataSource) {
+        super(dataSource);
+    }
+
+    @Override
+    public Integer getActive() {
+        return (int) getDataSource().getMetrics().activeCount();
+    }
+
+    @Override
+    public Integer getIdle() {
+        return (int) getDataSource().getMetrics().availableCount();
+    }
+
+    @Override
+    public Integer getMax() {
+        return getDataSource().getConfiguration().connectionPoolConfiguration().maxSize();
+    }
+
+    @Override
+    public Integer getMin() {
+        return getDataSource().getConfiguration().connectionPoolConfiguration().minSize();
+    }
+
+    @Override
+    public String getValidationQuery() {
+        return null;
+    }
+
+    @Override
+    public Boolean getDefaultAutoCommit() {
+        return getDataSource().getConfiguration().connectionPoolConfiguration().connectionFactoryConfiguration().autoCommit();
+    }
+}
---
+++ agroal-spring-boot-starter/src/main/java/io/agroal/springframework/boot/metrics/AgroalPoolDataSourceMetadataProviderConfiguration.java
@@ -0,0 +1,28 @@
+// Copyright (C) 2020 Red Hat, Inc. and individual contributors as indicated by the @author tags.
+// You may not use this file except in compliance with the Apache License, Version 2.0.
+
+package io.agroal.springframework.boot.metrics;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.jdbc.DataSourceUnwrapper;
+import org.springframework.boot.jdbc.metadata.DataSourcePoolMetadataProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.agroal.springframework.boot.AgroalDataSource;
+
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnClass(AgroalDataSource.class)
+public class AgroalPoolDataSourceMetadataProviderConfiguration {
+
+    @Bean
+    public DataSourcePoolMetadataProvider agroalPoolDataSourceMetadataProvider() {
+        return (dataSource) -> {
+            AgroalDataSource agroalDataSource = DataSourceUnwrapper.unwrap(dataSource, AgroalDataSource.class);
+            if (agroalDataSource != null) {
+                return new AgroalDataSourcePoolMetadata(agroalDataSource);
+            }
+            return null;
+        };
+    }
+}
