b745fa3b1b8ca5dcbc08875b34a16e0c1840c738
Benjamin Graf <benjamin.graf@gmx.net>
2023-05-23 14:45:21 +0200

    Fix SpringBoot related configuration bug and add spring to surefire groups

--- agroal-test/src/test/java/io/agroal/test/springframework/AgroalDataSourceConfigurationTests.java
+++ agroal-test/src/test/java/io/agroal/test/springframework/AgroalDataSourceConfigurationTests.java
@@ -1,27 +1,50 @@
+// Copyright (C) 2023 Red Hat, Inc. and individual contributors as indicated by the @author tags.
+// You may not use this file except in compliance with the Apache License, Version 2.0.
+
 package io.agroal.test.springframework;
 
+import dev.snowdrop.boot.narayana.autoconfigure.NarayanaConfiguration;
+import io.agroal.api.configuration.AgroalConnectionPoolConfiguration;
+import io.agroal.api.transaction.TransactionIntegration;
+import io.agroal.narayana.NarayanaTransactionIntegration;
 import io.agroal.springframework.boot.AgroalDataSource;
 import io.agroal.springframework.boot.AgroalDataSourceConfiguration;
 import io.agroal.springframework.boot.metrics.AgroalDataSourcePoolMetadata;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.annotation.UserConfigurations;
 import org.springframework.boot.jdbc.metadata.DataSourcePoolMetadata;
 import org.springframework.boot.jdbc.metadata.DataSourcePoolMetadataProvider;
 import org.springframework.boot.test.context.FilteredClassLoader;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.jta.JtaTransactionManager;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Map;
 import java.util.Objects;
 
+import static io.agroal.test.AgroalTestGroup.SPRING;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@Tag( SPRING )
 class AgroalDataSourceConfigurationTests {
+
+    private final static Logger LOG = LoggerFactory.getLogger(AgroalDataSourceConfigurationTests.class);
+
     private final Class<?>[] autoconfigurationsInWrongOrder = new Class<?>[]{
             DataSourceAutoConfiguration.class,
             AgroalDataSourceConfiguration.class
@@ -32,8 +55,9 @@ class AgroalDataSourceConfigurationTests {
 
     @DisplayName("Autoconfigurations will trigger in the correct order and the beans will be registered and created")
     @Test
-    void testAutoconfigureAgroalDataSource() {
-        runner.run(context -> {
+    void testAutoconfigureAgroalDataSourceWithMetrics() {
+        runner.withPropertyValues("spring.datasource.agroal.metrics=true")
+                .run(context -> {
             assertThat(context).hasSingleBean(AgroalDataSource.class);
 
             DataSource dataSource = context.getBean(AgroalDataSource.class);
@@ -45,7 +69,87 @@ class AgroalDataSourceConfigurationTests {
                     .findFirst()
                     .orElseThrow(() -> new IllegalStateException("AgroalDataSourcePoolMetaData not provided by any DataSourcePoolMetaDataProvider"));
             assertThat(metadata).isInstanceOf(AgroalDataSourcePoolMetadata.class);
+            assertThat(metadata.getActive()).isEqualTo(0);
+
+            try (Connection c = dataSource.getConnection()) {
+                LOG.info("Got connection {}", c);
+                assertThat(metadata.getActive()).isEqualTo(1);
+            }
         });
+    }
+
+    @DisplayName("Autoconfiguration will create DataSource with provided properties")
+    @Test
+    void testAutoconfigureAgroalDataSourceWithProperties() {
+        runner.withPropertyValues(
+                "spring.datasource.agroal.min-size=13",
+                "spring.datasource.agroal.initial-size=7",
+                "spring.datasource.agroal.max-size=37")
+                .run(context -> {
+                    AgroalDataSource dataSource = context.getBean(AgroalDataSource.class);
+                    AgroalConnectionPoolConfiguration poolConfiguration = dataSource.getConfiguration().connectionPoolConfiguration();
+
+                    assertThat(poolConfiguration.minSize()).isEqualTo(13);
+                    assertThat(poolConfiguration.initialSize()).isEqualTo(7);
+                    assertThat(poolConfiguration.maxSize()).isEqualTo(37);
+
+                    try (Connection c = dataSource.getConnection()) {
+                        LOG.info("Got connection {}", c);
+                    }
+                });
+    }
+
+    @DisplayName("Autoconfiguration will create DataSource with integration to Narayana")
+    @Test
+    void testAutoconfigureAgroalDataSourceWithNarayanaIntegration() {
+        runner.withPropertyValues("narayana.logDir=ObjectStore")
+                .withConfiguration(UserConfigurations.of(NarayanaConfiguration.class))
+                .run(context -> {
+                    AgroalDataSource dataSource = context.getBean(AgroalDataSource.class);
+                    TransactionIntegration transactionIntegration = dataSource.getConfiguration().connectionPoolConfiguration().transactionIntegration();
+
+                    assertThat(transactionIntegration).isInstanceOf(NarayanaTransactionIntegration.class);
+
+                    JtaTransactionManager txManager = context.getBean(JtaTransactionManager.class);
+                    assertThat(txManager).isNotNull();
+
+                    TransactionStatus txStatus = txManager.getTransaction(null);
+                    assertThat(TransactionSynchronizationManager.isActualTransactionActive()).isTrue();
+
+                    Connection c = dataSource.getConnection();
+                    LOG.info("Got connection {}", c);
+
+                    assertThatThrownBy(() -> c.setAutoCommit( true )).isInstanceOf(SQLException.class);
+                    assertThat(c.getAutoCommit()).isFalse();
+                    assertThat(c.unwrap(Connection.class)).isEqualTo(dataSource.getConnection().unwrap(Connection.class));
+
+                    txManager.rollback(txStatus);
+                    assertThat(c.isClosed()).isTrue();
+                });
+    }
+
+    @DisplayName("Autoconfiguration will create connectable DataSource bound to JNDI")
+    @Test
+    void testAutoconfigureAgroalDataSourceBoundToJndi() {
+        System.setProperty("java.naming.factory.initial", "org.osjava.sj.SimpleJndiContextFactory");
+        System.setProperty("org.osjava.sj.jndi.shared", "true");
+        System.setProperty("org.osjava.sj.jndi.ignoreClose", "true");
+        runner.withConfiguration(UserConfigurations.of(NarayanaConfiguration.class))
+                .withPropertyValues(
+                        "narayana.logDir=ObjectStore",
+                        "spring.datasource.agroal.connectable=true",
+                        "spring.datasource.jndi-name=java:comp/env/jdbc/test")
+                .run(context -> {
+                    InitialContext ic = new InitialContext();
+                    Object dataSource = ic.lookup("java:comp/env/jdbc/test");
+
+                    assertThat(dataSource).isNotNull();
+                    assertThat(dataSource).isInstanceOf(AgroalDataSource.class);
+
+                    try (Connection c = ((AgroalDataSource) dataSource).getConnection()) { 
+                        LOG.info("Got connection {}", c);
+                    }
+                });
     }
 
     @DisplayName("Autoconfiguration will not trigger when AgroalDataSource is not on the classpath")
--- agroal-test/src/test/java/io/agroal/test/springframework/metrics/AgroalDataSourcePoolMetricsAutoConfigurationTests.java
+++ agroal-test/src/test/java/io/agroal/test/springframework/metrics/AgroalDataSourcePoolMetricsAutoConfigurationTests.java
@@ -1,12 +1,19 @@
+// Copyright (C) 2023 Red Hat, Inc. and individual contributors as indicated by the @author tags.
+// You may not use this file except in compliance with the Apache License, Version 2.0.
+
 package io.agroal.test.springframework.metrics;
 
 import io.agroal.springframework.boot.AgroalDataSource;
 import io.agroal.springframework.boot.AgroalDataSourceConfiguration;
 import io.agroal.springframework.boot.metrics.AgroalDataSourcePoolMetricsAutoConfiguration;
+import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.MeterBinder;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.autoconfigure.metrics.CompositeMeterRegistryAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.metrics.MetricsAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.metrics.export.simple.SimpleMetricsExportAutoConfiguration;
@@ -15,12 +22,17 @@ import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.test.context.FilteredClassLoader;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 
+import java.sql.Connection;
 import java.util.Map;
 
+import static io.agroal.test.AgroalTestGroup.SPRING;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@Tag( SPRING )
 class AgroalDataSourcePoolMetricsAutoConfigurationTests {
+
+    private final static Logger LOG = LoggerFactory.getLogger(AgroalDataSourcePoolMetricsAutoConfigurationTests.class);
 
     private final Class<?>[] autoconfigurationsInWrongOrder = new Class<?>[]{
             AgroalDataSourcePoolMetricsAutoConfiguration.class,
@@ -36,7 +48,7 @@ class AgroalDataSourcePoolMetricsAutoConfigurationTests {
     @DisplayName("Autoconfigurations will trigger in the correct order and the beans will be registered and created")
     @Test
     void testAutoconfigureAgroalDataSourceMeterBinder() {
-        runner
+        runner.withPropertyValues("spring.datasource.agroal.metrics=true")
                 .run(context -> {
                     assertThat(context).hasSingleBean(MeterBinder.class);
                     assertThat(context).hasSingleBean(AgroalDataSource.class);
@@ -45,6 +57,14 @@ class AgroalDataSourcePoolMetricsAutoConfigurationTests {
                     AgroalDataSource dataSource = context.getBean(AgroalDataSource.class);
 
                     assertThat(meterBinder).hasFieldOrPropertyWithValue("dataSources", Map.of("dataSource", dataSource));
+                    MeterRegistry registry = context.getBean(MeterRegistry.class);
+                    Gauge gauge = registry.get("agroal.connections.creation.count").gauge();
+                    assertThat(gauge.value()).isEqualTo(0.0);
+
+                    try (Connection c = dataSource.getConnection()) {
+                        LOG.info("Got connection {}", c);
+                        assertThat(gauge.value()).isEqualTo(1.0);
+                    }
                 });
     }
 
