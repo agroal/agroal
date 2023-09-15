707aa09d809db354980a6d4e0ddd60fa5b7b93d2
barreiro <lbbbarreiro@gmail.com>
2023-09-16 00:30:53 +0100

    AG-220 - Do not register connectable datasource for XA Recovery

--- agroal-spring-boot-starter/src/main/java/io/agroal/springframework/boot/AgroalDataSourceConfiguration.java
+++ agroal-spring-boot-starter/src/main/java/io/agroal/springframework/boot/AgroalDataSourceConfiguration.java
@@ -18,13 +18,14 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DatabaseDriver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.jta.JtaTransactionManager;
 import org.springframework.util.StringUtils;
 
 import javax.sql.DataSource;
+
+import static org.springframework.boot.jdbc.DatabaseDriver.fromJdbcUrl;
 
 /**
  * @author <a href="lbarreiro@redhat.com">Luis Barreiro</a>
@@ -47,33 +48,30 @@ public class AgroalDataSourceConfiguration {
 
     @Bean
     @ConfigurationProperties( prefix = "spring.datasource.agroal" )
+    @SuppressWarnings( {"HardcodedFileSeparator", "StringConcatenation"} )
     public AgroalDataSource dataSource(
             DataSourceProperties properties,
-            @Value("${spring.datasource.agroal.connectable:false}") boolean connectable,
-            @Value("${spring.datasource.agroal.firstResource:false}") boolean firstResource,
+            @Value( "${spring.datasource.agroal.connectable:false}" ) boolean connectable,
+            @Value( "${spring.datasource.agroal.firstResource:false}" ) boolean firstResource,
             ObjectProvider<AgroalDataSourceJndiBinder> jndiBinder) {
+
         AgroalDataSource dataSource = properties.initializeDataSourceBuilder().type( AgroalDataSource.class ).build();
         if ( !StringUtils.hasLength( properties.getDriverClassName() ) ) {
-            DatabaseDriver driver = DatabaseDriver.fromJdbcUrl( properties.determineUrl() );
-            if ( connectable ) {
-                dataSource.setDriverClassName( driver.getDriverClassName() );
-            } else {
-                dataSource.setDriverClassName( driver.getXaDataSourceClassName() );
-            }
+            dataSource.setDriverClassName( connectable ? fromJdbcUrl( properties.determineUrl() ).getDriverClassName() : fromJdbcUrl( properties.determineUrl() ).getXaDataSourceClassName() );
         }
         String name = properties.determineDatabaseName();
         dataSource.setName( name );
         String jndiName = properties.getJndiName() != null ? properties.getJndiName() : "java:comp/env/jdbc/" + name;
-        if ( jtaPlatform != null && jtaPlatform.getTransactionManager() != null && jtaPlatform.getTransactionSynchronizationRegistry() != null) {
-            NarayanaTransactionIntegration transactionIntegration = new NarayanaTransactionIntegration(
+        if ( jtaPlatform != null && jtaPlatform.getTransactionManager() != null && jtaPlatform.getTransactionSynchronizationRegistry() != null ) {
+            dataSource.setJtaTransactionIntegration( new NarayanaTransactionIntegration(
                     jtaPlatform.getTransactionManager(),
                     jtaPlatform.getTransactionSynchronizationRegistry(),
                     jndiName,
                     connectable,
                     firstResource,
-                    recoveryRegistry );
-            dataSource.setJtaTransactionIntegration( transactionIntegration );
-            if ( connectable && jndiBinder.getIfUnique(DefaultAgroalDataSourceJndiBinder::new).bindToJndi(jndiName, dataSource)) {
+                    connectable ? null : recoveryRegistry )
+            );
+            if ( connectable && jndiBinder.getIfUnique( DefaultAgroalDataSourceJndiBinder::new ).bindToJndi( jndiName, dataSource ) ) {
                 logger.info( "Bind DataSource {} as {} to JNDI registry", name, jndiName );
             }
         }
