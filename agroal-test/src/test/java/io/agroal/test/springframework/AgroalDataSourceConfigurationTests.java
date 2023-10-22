a0a7fbacb9b52996235ebb8d717556a1894340b6
Ronald Mik <ronaldmik@gmail.com>
2023-10-22 11:41:32 +0200

    AG-223: Changed AgroalPoolDataSourceMetadataProviderConfiguration to normal @Configuration class that is imported with AgroalDataSourceConfiguration

--- agroal-test/src/test/java/io/agroal/test/springframework/AgroalDataSourceConfigurationTests.java
+++ agroal-test/src/test/java/io/agroal/test/springframework/AgroalDataSourceConfigurationTests.java
@@ -2,13 +2,21 @@ package io.agroal.test.springframework;
 
 import io.agroal.springframework.boot.AgroalDataSource;
 import io.agroal.springframework.boot.AgroalDataSourceConfiguration;
+import io.agroal.springframework.boot.metrics.AgroalDataSourcePoolMetadata;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.metadata.DataSourcePoolMetadata;
+import org.springframework.boot.jdbc.metadata.DataSourcePoolMetadataProvider;
 import org.springframework.boot.test.context.FilteredClassLoader;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+
+import javax.sql.DataSource;
+
+import java.util.Map;
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -25,7 +33,19 @@ class AgroalDataSourceConfigurationTests {
     @DisplayName("Autoconfigurations will trigger in the correct order and the beans will be registered and created")
     @Test
     void testAutoconfigureAgroalDataSource() {
-        runner.run(context -> assertThat(context).hasSingleBean(AgroalDataSource.class));
+        runner.run(context -> {
+            assertThat(context).hasSingleBean(AgroalDataSource.class);
+
+            DataSource dataSource = context.getBean(AgroalDataSource.class);
+            Map<String, DataSourcePoolMetadataProvider> metadataProviders = context.getBeansOfType(DataSourcePoolMetadataProvider.class);
+
+            DataSourcePoolMetadata metadata = metadataProviders.values().stream()
+                    .map(provider -> provider.getDataSourcePoolMetadata(dataSource))
+                    .filter(Objects::nonNull)
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalStateException("AgroalDataSourcePoolMetaData not provided by any DataSourcePoolMetaDataProvider"));
+            assertThat(metadata).isInstanceOf(AgroalDataSourcePoolMetadata.class);
+        });
     }
 
     @DisplayName("Autoconfiguration will not trigger when AgroalDataSource is not on the classpath")
