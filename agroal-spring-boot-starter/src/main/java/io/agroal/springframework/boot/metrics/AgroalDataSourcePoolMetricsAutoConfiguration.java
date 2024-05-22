08c28e46251e2d5a61aee46531c0004f522cfcd8
Benjamin Graf <benjamin.graf@gmx.net>
2024-05-22 17:04:36 +0200

    AG-241 - Switch from property injection to constructor injection

--- agroal-spring-boot-starter/src/main/java/io/agroal/springframework/boot/metrics/AgroalDataSourcePoolMetricsAutoConfiguration.java
+++ agroal-spring-boot-starter/src/main/java/io/agroal/springframework/boot/metrics/AgroalDataSourcePoolMetricsAutoConfiguration.java
@@ -5,9 +5,10 @@ package io.agroal.springframework.boot.metrics;
 
 import io.agroal.api.AgroalDataSource;
 import io.agroal.pool.DefaultMetricsRepository;
-import io.agroal.springframework.boot.AgroalDataSourceConfiguration;
+import io.agroal.springframework.boot.AgroalDataSourceAutoConfiguration;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.MeterBinder;
+
 import org.springframework.boot.actuate.autoconfigure.metrics.CompositeMeterRegistryAutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
@@ -16,9 +17,10 @@ import org.springframework.boot.jdbc.DataSourceUnwrapper;
 import org.springframework.context.annotation.Bean;
 
 import javax.sql.DataSource;
+
 import java.util.Map;
 
-@AutoConfiguration(after = {AgroalDataSourceConfiguration.class, CompositeMeterRegistryAutoConfiguration.class})
+@AutoConfiguration(after = {AgroalDataSourceAutoConfiguration.class, CompositeMeterRegistryAutoConfiguration.class})
 @ConditionalOnClass({AgroalDataSource.class, MeterRegistry.class})
 @ConditionalOnBean({AgroalDataSource.class, MeterRegistry.class})
 public class AgroalDataSourcePoolMetricsAutoConfiguration {
