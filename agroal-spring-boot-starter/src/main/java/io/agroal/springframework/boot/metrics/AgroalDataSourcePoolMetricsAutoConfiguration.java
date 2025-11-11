f9497c3959af88cf2215e96e3879af0ea35455d6
Benjamin Graf <benjamin.graf@gmx.net>
2025-11-11 12:18:26 +0100

    AG-271 - Major update to support Spring Boot 4.0.0

--- agroal-spring-boot-starter/src/main/java/io/agroal/springframework/boot/metrics/AgroalDataSourcePoolMetricsAutoConfiguration.java
+++ agroal-spring-boot-starter/src/main/java/io/agroal/springframework/boot/metrics/AgroalDataSourcePoolMetricsAutoConfiguration.java
@@ -9,44 +9,46 @@ import io.agroal.springframework.boot.AgroalDataSourceAutoConfiguration;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.MeterBinder;
 
-import org.springframework.boot.actuate.autoconfigure.metrics.CompositeMeterRegistryAutoConfiguration;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.SimpleAutowireCandidateResolver;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.jdbc.DataSourceUnwrapper;
+import org.springframework.boot.micrometer.metrics.autoconfigure.CompositeMeterRegistryAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 import javax.sql.DataSource;
 
 import java.util.Map;
 
-@AutoConfiguration(after = {AgroalDataSourceAutoConfiguration.class, CompositeMeterRegistryAutoConfiguration.class})
-@ConditionalOnClass({AgroalDataSource.class, MeterRegistry.class})
-@ConditionalOnBean({AgroalDataSource.class, MeterRegistry.class})
+@AutoConfiguration( after = { AgroalDataSourceAutoConfiguration.class, CompositeMeterRegistryAutoConfiguration.class } )
+@ConditionalOnClass( { AgroalDataSource.class, MeterRegistry.class } )
+@ConditionalOnBean( { AgroalDataSource.class, MeterRegistry.class } )
 public class AgroalDataSourcePoolMetricsAutoConfiguration {
 
     @Bean
-    AgroalDataSourceMeterBinder agroalDataSourceMeterBinder(Map<String, ? extends DataSource> dataSources) {
-        return new AgroalDataSourceMeterBinder(dataSources);
+    AgroalDataSourceMeterBinder agroalDataSourceMeterBinder( ConfigurableListableBeanFactory beanFactory ) {
+        return new AgroalDataSourceMeterBinder( SimpleAutowireCandidateResolver.resolveAutowireCandidates( beanFactory, DataSource.class, false, true ) );
     }
 
     public static class AgroalDataSourceMeterBinder implements MeterBinder {
 
-        private final Map<String, ? extends DataSource> dataSources;
+        private final Map<String, DataSource> dataSources;
 
-        AgroalDataSourceMeterBinder(Map<String, ? extends DataSource> dataSources) {
+        AgroalDataSourceMeterBinder( Map<String, DataSource> dataSources ) {
             this.dataSources = dataSources;
         }
 
         @Override
-        public void bindTo(MeterRegistry registry) {
-            dataSources.forEach((name, dataSource) -> bindDataSourceToRegistry(name, dataSource, registry));
+        public void bindTo( MeterRegistry registry ) {
+            dataSources.forEach( ( name, dataSource ) -> bindDataSourceToRegistry( name, dataSource, registry ) );
         }
 
-        private void bindDataSourceToRegistry(String name, DataSource dataSource, MeterRegistry registry) {
-            AgroalDataSource agroalDataSource = DataSourceUnwrapper.unwrap(dataSource, AgroalDataSource.class);
-            if (agroalDataSource != null && agroalDataSource.getMetrics() instanceof DefaultMetricsRepository) {
-                new AgroalDataSourcePoolMetrics(name, agroalDataSource).bindTo(registry);
+        private void bindDataSourceToRegistry( String name, DataSource dataSource, MeterRegistry registry ) {
+            AgroalDataSource agroalDataSource = DataSourceUnwrapper.unwrap( dataSource, AgroalDataSource.class );
+            if ( agroalDataSource != null && agroalDataSource.getMetrics() instanceof DefaultMetricsRepository ) {
+                new AgroalDataSourcePoolMetrics( name, agroalDataSource ).bindTo( registry );
             }
         }
     }
