fa0f90f11762675cd383c9cbd0c50d5b3709c7fb
George Gastaldi <gegastaldi@gmail.com>
2025-11-19 15:53:56 -0300

    Create Datasources utility class

--- agroal-integration-tests/src/test/java/io/agroal/tests/PostgreSQLTest.java
+++ agroal-integration-tests/src/test/java/io/agroal/tests/PostgreSQLTest.java
@@ -1,9 +1,6 @@
 package io.agroal.tests;
 
 import io.agroal.api.AgroalDataSource;
-import io.agroal.api.configuration.supplier.AgroalDataSourceConfigurationSupplier;
-import io.agroal.api.security.NamePrincipal;
-import io.agroal.api.security.SimplePassword;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +10,8 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 
 import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
 
 @Testcontainers
 public class PostgreSQLTest {
@@ -24,23 +23,12 @@ public class PostgreSQLTest {
 
     @BeforeEach
     void setupDataSource() throws SQLException {
-        AgroalDataSourceConfigurationSupplier configurationSupplier = new AgroalDataSourceConfigurationSupplier()
-                .connectionPoolConfiguration(cp -> cp
-                        .maxSize(1)
-                        .connectionFactoryConfiguration(cf -> cf
-                                .connectionProviderClass(org.postgresql.Driver.class)
-                                .jdbcUrl(postgres.getJdbcUrl())
-                                .principal(new NamePrincipal(postgres.getUsername()))
-                                .credential(new SimplePassword(postgres.getPassword()))
-                        )
-                );
-        this.datasource = AgroalDataSource.from(configurationSupplier);
+        this.datasource = Datasources.createDataSource(postgres);
     }
-
 
     @Test
     void shouldConnect() {
-        Assertions.assertThatCode(() -> datasource.getConnection()).doesNotThrowAnyException();
+        assertThatNoException().isThrownBy(() -> datasource.getConnection().close());
     }
 
     @AfterEach
