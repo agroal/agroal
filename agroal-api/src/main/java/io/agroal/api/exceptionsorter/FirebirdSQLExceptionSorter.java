69c85589936f7a908e6ea82024ca5ed19fd32d5d
Erwan Tanajaya <r_1gm@yahoo.com>
2026-01-02 16:43:41 +0700

    AG-283 : Basic FirebirdSql support

---
+++ agroal-api/src/main/java/io/agroal/api/exceptionsorter/FirebirdSQLExceptionSorter.java
@@ -0,0 +1,19 @@
+// Copyright (C) 2018 Red Hat, Inc. and individual contributors as indicated by the @author tags.
+// You may not use this file except in compliance with the Apache License, Version 2.0.
+package io.agroal.api.exceptionsorter;
+
+import io.agroal.api.configuration.AgroalConnectionPoolConfiguration.ExceptionSorter;
+import java.sql.SQLException;
+
+/**
+ * Exception sorter for FirebirdSQL databases.
+ *
+ */
+public class FirebirdSQLExceptionSorter implements ExceptionSorter {
+
+    @Override
+    @SuppressWarnings("MagicNumber")
+    public boolean isFatal(SQLException e) {
+        return e.getSQLState() != null && e.getSQLState().startsWith("08");
+    }
+}
