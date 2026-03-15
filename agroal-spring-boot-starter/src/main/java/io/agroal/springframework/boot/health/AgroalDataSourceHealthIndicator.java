146e11b372bce86a1cf9cb3440eee88036624640
Benjamin Graf <benjamin.graf@gmx.net>
2026-03-15 16:55:44 +0100

    AG-298 - Determine database from metadata if no jdbcUrl supplied

--- agroal-spring-boot-starter/src/main/java/io/agroal/springframework/boot/health/AgroalDataSourceHealthIndicator.java
+++ agroal-spring-boot-starter/src/main/java/io/agroal/springframework/boot/health/AgroalDataSourceHealthIndicator.java
@@ -8,23 +8,41 @@ import io.agroal.api.configuration.AgroalConnectionPoolConfiguration;
 import org.springframework.boot.health.contributor.AbstractHealthIndicator;
 import org.springframework.boot.health.contributor.Health;
 import org.springframework.boot.health.contributor.Status;
+import org.springframework.boot.jdbc.DatabaseDriver;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static io.agroal.api.configuration.AgroalConnectionPoolConfiguration.TransactionRequirement.OFF;
+import static org.springframework.boot.jdbc.DatabaseDriver.UNKNOWN;
 import static org.springframework.boot.jdbc.DatabaseDriver.fromJdbcUrl;
 
 public class AgroalDataSourceHealthIndicator extends AbstractHealthIndicator {
 
     private final AgroalDataSource dataSource;
+    private final Object database;
 
     public AgroalDataSourceHealthIndicator( AgroalDataSource dataSource ) {
         this.dataSource = dataSource;
+        AgroalConnectionPoolConfiguration agroalConnectionPoolConfiguration = dataSource.getConfiguration().connectionPoolConfiguration();
+        DatabaseDriver driver = fromJdbcUrl( agroalConnectionPoolConfiguration.connectionFactoryConfiguration().jdbcUrl() );
+        if ( driver == UNKNOWN && agroalConnectionPoolConfiguration.transactionRequirement() == OFF ) {
+            this.database = new JdbcTemplate( dataSource ).execute( this::getProduct );
+        } else {
+            this.database = driver;
+        }
+    }
+
+    private String getProduct( Connection connection ) throws SQLException {
+        return connection.getMetaData().getDatabaseProductName();
     }
 
     @Override
     protected void doHealthCheck( Health.Builder builder ) throws Exception {
-        AgroalConnectionPoolConfiguration agroalConnectionPoolConfiguration = dataSource.getConfiguration().connectionPoolConfiguration();
         builder.withDetail( "provider", "Agroal" );
-        builder.withDetail( "database", fromJdbcUrl( agroalConnectionPoolConfiguration.connectionFactoryConfiguration().jdbcUrl() ) );
-        builder.withDetail( "validator", agroalConnectionPoolConfiguration.connectionValidator().getClass() );
+        builder.withDetail( "database", database );
+        builder.withDetail( "validator", dataSource.getConfiguration().connectionPoolConfiguration().connectionValidator().getClass() );
         boolean healthy = dataSource.isHealthy( false );
         builder.status( (healthy) ? Status.UP : Status.DOWN );
     }
