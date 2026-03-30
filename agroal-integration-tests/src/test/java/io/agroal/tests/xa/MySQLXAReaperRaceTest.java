60a8e2277958515e17716a86b4b7b9460ad91292
marco sappe griot <msappegr@ibm.com>
2026-03-30 10:24:39 +0200

    AG-304 -  reproducer
    Image names are now set as properties
    
    You can now override any image on the command line:
      mvn -Pit -Dmssql.testcontainer.image=mcr.microsoft.com/mssql/server:2022-CU22-ubuntu-22.04 verify

---
+++ agroal-integration-tests/src/test/java/io/agroal/tests/xa/MySQLXAReaperRaceTest.java
@@ -0,0 +1,41 @@
+package io.agroal.tests.xa;
+
+import org.jboss.byteman.contrib.bmunit.BMUnitConfig;
+import org.junit.jupiter.api.Tag;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.mysql.MySQLContainer;
+
+/**
+ * XA reaper race integration test against MySQL using Testcontainers.
+ * Uses SLEEP() for slow SQL that holds the read lock past the transaction timeout.
+ */
+@Tag( "testcontainers" )
+@Testcontainers
+@BMUnitConfig(debug = true)
+class MySQLXAReaperRaceTest extends XAReaperRaceTestBase {
+
+    @Container
+    static MySQLContainer mysql = new MySQLContainer( System.getProperty( "mysql.testcontainer.image", "mysql:8.0" ) );
+
+    @Override
+    String xaDataSourceClassName() {
+        return "com.mysql.cj.jdbc.MysqlXADataSource";
+    }
+
+    @Override
+    JdbcDatabaseContainer<MySQLContainer>  container() {
+        return mysql;
+    }
+
+    @Override
+    String slowSQL() {
+        return "SELECT SLEEP(4)";
+    }
+
+    @Override
+    String createTableDDL() {
+        return "CREATE TABLE IF NOT EXISTS xa_reaper_test ( id INT PRIMARY KEY, val VARCHAR(100) ) ENGINE=InnoDB";
+    }
+}
